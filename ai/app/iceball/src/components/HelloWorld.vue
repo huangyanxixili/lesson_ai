fae12679b46118c3434f350a702aaecaa3f0bcb3
huangyanxixili <1954058551@qq.com>
2026-01-06 18:51:15 +0800

    写文章的测试代码

--- ai/app/iceball/src/components/HelloWorld.vue
+++ ai/app/iceball/src/components/HelloWorld.vue
@@ -1,43 +1,26 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="container">
+    <!-- 输入区域 -->
+    <div class="input">
+      <div class="file-input">
+        <input type="file" ref="uploadImage" accept="image/*" @change="updateImageData" required />
+      </div>
+      <img :src="imgPreview" alt="" v-if="imgPreview" />
+      ...
+    </div>
+    <div class="output">
+      <div class="generated">
+        <img :src="imgUrl" alt="" v-if="imgUrl">
+        <div v-if="status">{{ status }}</div>
+      </div>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
 </style>
