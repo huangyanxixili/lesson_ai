b142ce9928211677088f7f418535bc039bb9672a
huangyanxixili <1954058551@qq.com>
2026-02-17 12:21:46 +0800

    加深理解流式输出

--- project/ai_fullstack/frontend/notes/src/mock/chat.js
+++ project/ai_fullstack/frontend/notes/src/mock/chat.js
@@ -32,7 +32,7 @@ export default [
                     res.setHeader('Transfer-Encoding', 'chunked');
                     // vercel ai sdk 特制头
                     res.setHeader('x-vercel-ai-data-stream', 'v1');
-                    const response = await fetch('http://api.deepseek.com/v1/chat/completions', {
+                    const response = await fetch('https://api.deepseek.com/v1/chat/completions', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
@@ -45,31 +45,46 @@ export default [
                         })
                     })
                     if (!response.body) throw new Error("No response body");
-                    // SSE 二进制流 reader 对象 接根管子一样
-                    const reader = response.body.getReader();
-                    // 将 ArrayBuffer 或 TypedArray 解码为字符串
+                    // SSE 二进制流 reader 对象
+                    // LLM 输出和解析之间接根管子一样 
+                    // reader对象读取，llm输出的token流
+                    const reader = response.body.getReader(); // token 
+                    // 通过 TextDecoder（文本解码器） 将 Uint8Array字节数据 解码为 可读的UTF-8字符串
                     const decoder = new TextDecoder();
                     while(true) {
-                        const { done, value } = await reader.read();
-                        // console.log(done, value, '------------------------');
+                        // llm 的这一次的生成，被读取了
+                        // 事件，有新的token生成了
+                        const { done, value } = await reader.read(); 
+                        console.log(done, value, '------------------------');
+                        // done：是否完成, value：当前读取到的二进制数据块
                         if (done) break;
+                        // 解析出 token => 字符串 LLM 内部 数学向量
+                        // chunk 是一个文本行，其开头data是前缀，{}内是json字符串
+                        // choices[0].delta.content就是拿到的 token 字符串
                         const chunk = decoder.decode(value);
-                        // console.log(chunk, '------------------------');
-                        const lines = chunk.split('\n');
-                        for (let line of lines) {
+                        console.log(chunk, '------------------------'); // chunk包含了 token 信息
+                        // 其中的 delta 是增量，每次拿到的 delta.content，就是这一小次新增的那几字符，而不是完整句子
+                        const lines = chunk.split('\n'); // 按行分割，拿到每一行有效数据
+                        for (let line of lines) { // 不需要用到下标就用for of，计数循环比较机械
+                            // startsWith es6的语法
+                            // 判断 字符串开头是否包含指定的子字符串，更优雅
+                            // data: [DONE] 结束信号
                             if (line.startsWith('data: ') && line !== 'data: [DONE]') {
                                 try {
                                     const data = JSON.parse(line.slice(6));
+                                    // ?. 代码的健壮性
                                     const content = data.choices[0]?.delta?.content || '';
                                     if (content) {
+                                        // 发送给前端 SSE核心
                                         res.write(`0:${JSON.stringify(content)}\n`);
                                     }
                                 } catch(err) {
-                                    console.log(err, '------------------------');
+
                                 }
                             }
                         }
                     }
+                    // 结束响应
                     res.end();
                 } catch(err) {
 
